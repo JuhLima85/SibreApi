9e6145946f746eb5c93931fb8ab6e15ddd0adbb7
JuhLima85 <limajuh17@gmail.com>
2025-06-03 15:18:55 -0300

    Criação do Projeto

---
+++ src/test/java/br/com/sibre/nova/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sibre.nova.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
